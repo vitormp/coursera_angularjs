7b62e392b3bea5298a5ea8d89be81d35f7f14088
Vitor Monteiro Puente <vitor.m.p22@gmail.com>
2016-09-09 20:01:59 -0300

    some refactoring

--- week1/assignment1/app.js
+++ week1/assignment1/app.js
@@ -8,28 +8,22 @@
 
   function LunchCheckController($scope) {
     $scope.itemsList = "";
-    $scope.listOfDishes = [];
     $scope.lunchAdvise = "";
 
 
     $scope.verifyFoodList = function (){
-      console.log($scope.itemsList);
-      console.log($scope.itemsList.trim());
-      if ($scope.itemsList.trim() == "") {
-        $scope.lunchAdvise =  "Please enter data first";
-        return;
-      }
+      $scope.lunchAdvise =  "Too much!";
 
-      $scope.listOfDishes = $scope.itemsList.split(' ');
-      var numberOfDishes = $scope.listOfDishes.length;
-      console.log($scope.listOfDishes.length);
+      var listOfDishes = $scope.itemsList.split(' ');
+      var numberOfDishes = listOfDishes.length;
 
       if (numberOfDishes <= 3) {
         $scope.lunchAdvise =  "Enjoy!";
-        return;
       }
 
-      $scope.lunchAdvise =  "Too much!";
+      if ($scope.itemsList.trim() == "") {
+        $scope.lunchAdvise =  "Please enter data first";
+      }
     };
   }
 
