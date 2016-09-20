05df8dd42acbbc8decdaffae851702e52bfdbe4f
Vitor Monteiro Puente <vitor.m.p22@gmail.com>
2016-09-19 22:41:04 -0300

    contrller and service structures

--- week2/assignment2/scripts/app.js
+++ week2/assignment2/scripts/app.js
@@ -6,15 +6,22 @@
   .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyShoppingController() {
-
+    var toBuy = this;
   }
 
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtShoppingController() {
+    var alreadyBought = this;
 
   }
 
   function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyItems = [];
+    var boughtItems = [];
 
   }
 })();
